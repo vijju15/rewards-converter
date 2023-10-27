56fcaa7df56ef20b38e06e5c9f66572a03744913
vijay kumar <vijay@vijay.com>
2023-10-26 21:38:41 -0500

    Added RewardValue class

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -8,13 +8,13 @@ public class RewardsConverter {
         var input_value = scanner.nextLine();
         double cashValue;
         try {
-            cashValue = Double.parseDouble(input_value);
+            cashValue = Double.parseDouble(input_value) ;
         } catch (NumberFormatException exception) {
             System.out.println("Could not parse input value as a double, exiting");
             return;
         }
         System.out.println("converting $" + input_value + " to miles");
-        var rewardsValue = new RewardValue(cashValue);
+        var rewardsValue = new RewardValue(cashValue );
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
     }
 }
